f3f1d0464e0a18a2f19e6b70416b2b185e7c55df
Long Yonggang <yonggang.long@nokia-sbell.com>
2019-05-22 17:26:23 +0800

    regular

--- text-classification/train_regular_model.py
+++ text-classification/train_regular_model.py
@@ -35,20 +35,27 @@ def train_svc_model(data):
 def train_multiple_models(data):
     (train_texts, train_labels), (val_texts, val_labels) = data
     x_train, x_val = vectorize_data.tfidf_vectorize(train_texts, train_labels, val_texts)
-    x_train = x_train.toarray()
-    x_val = x_val.toarray()
+    # x_train = x_train.toarray()
+    # x_val = x_val.toarray()
 
     names = [
-             "Nearest Neighbors", "Linear SVM", "RBF SVM", 
-             "Gaussian Process",
+            #  "RBF SVM", 
+            #  "Gaussian Process",
              "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
              "Naive Bayes", "QDA"
             ]
     classifiers = [
-                    KNeighborsClassifier(8),
-                    SVC(kernel="linear", C=0.025),
-                    SVC(gamma=2, C=300),
-                    GaussianProcessClassifier(1.0 * RBF(1.0)),
+                    # SVC(C=300,
+                    #     kernel='rbf',
+                    #     gamma=1.5, 
+                    #     shrinking=True, 
+                    #     tol=0.001, 
+                    #     cache_size=1000,
+                    #     class_weight=None,
+                    #     max_iter=-1, 
+                    #     decision_function_shape='ovr',
+                    #     random_state=42),
+                    # GaussianProcessClassifier(1.0 * RBF(1.0)),
                     DecisionTreeClassifier(max_depth=5),
                     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
                     MLPClassifier(alpha=1),
